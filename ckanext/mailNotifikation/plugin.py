4f826dc790f5cc8e65fb8ee48a08046d63ef505f
tonnyandersen <toan@aarhus.dk>
2016-03-01 11:16:01 +0100

    Update plugin.py

--- ckanext/mailNotifikation/plugin.py
+++ ckanext/mailNotifikation/plugin.py
@@ -10,11 +10,11 @@ class MailnotifikationPlugin(plugins.SingletonPlugin):
     def update_config(self, config_):
         toolkit.add_template_directory(config_, 'templates')
         toolkit.add_public_directory(config_, 'controllers')
+        toolkit.add_public_directory(config_, 'public')
 
     def before_map(self, map):    
-        map.connect('/user/register',
-                    controller='ckanext.imailNotifikation.controllers.user:CustomUserController',
-                    action='register')   
+        map.connect('/user/register',controller='ckanext.mailNotifikation.controllers.user:CustomUserController',action='register') 
+        map.connect('/answers',controller='ckanext.mailNotifikation.controllers.answer:AnswersController', action='index')
         return map
   
 	
